262efdfcf0843547a942e1df38397fddeb7e1645
olegfox <info@olegfox.com>
2015-11-23 15:34:15 +0300

    last commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,9 +7,9 @@ module.exports = function(grunt) {
         shopify: {
             options: {
                 //You can find these in your Shopify Admin under Apps then look at the bottom for Create a private API Key
-                api_key: "6406b8b6be720a36ed4f76e2c21c5d77",
-                password: "05940372532f29624252d884d33ff7ab",
-                url: "olegfox.myshopify.com",
+                api_key: "de724a535ce1b6ad2f4f369bb44390da",
+                password: "740fef529d18ff50f6bbb9835252e9d3",
+                url: "tattooist-shop.myshopify.com",
                 base: 'shop/'
             }
         },
